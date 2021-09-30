d897d8b6fee5ad4e3958f4aea1258c89b4deef2b
darkergg <luisgnio90@gmail.com>
2021-09-29 23:12:15 -0600

    refactor(com.models/usuario)

--- src/com/models/Usuario.java
+++ src/com/models/Usuario.java
@@ -11,5 +11,32 @@ package com.models;
  * @author isram
  */
 public class Usuario {
+    String username; 
+    String password;
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Usuario() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
